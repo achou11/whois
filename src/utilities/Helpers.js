a6e488a035580b3d4078909c4ef7b6a311953b59
achou11 <chouandrewo@gmail.com>
2018-02-27 15:08:41 -0600

    small updates

--- src/utilities/Helpers.js
+++ src/utilities/Helpers.js
@@ -9,10 +9,22 @@ module.exports = {
 
     changeTheme() {
 
+        // Change address bar theme
+        let addr_bar = document.querySelector('meta[name="theme-color"]').content
+
+        if (addr_bar == '#363636') {
+            document.querySelector('meta[name="theme-color"]').content = '#f1f1f1'
+        } else {
+            document.querySelector('meta[name="theme-color"]').content = '#363636'
+        }
+        
+
+        // Change body theme
         let currentTheme = document.body.className
-        document.body.className = currentTheme == 'theme-dark' ? 'theme-light' : 'theme-dark';
+        document.body.className = currentTheme == 'theme-dark' ? 'theme-light' : 'theme-dark'
 
 
+        // Change map border color
         if (document.getElementById('map-container')) {
 
             let mapBorderColor = document.getElementById('map-container')
@@ -29,31 +41,27 @@ module.exports = {
             }
         }
 
+        
+        // Change nav border color
         if (document.getElementById('nav-bar')) {
             let navBar = document.getElementById('nav-bar')
 
-            switch (navBar.classList.contains('background-light')) {
-                case true:
-                    navBar.classList.add('background-dark')
-                    navBar.classList.remove('background-light')
+            if (navBar.classList.contains('background-light')) {
+                navBar.classList.add('background-dark')
+                navBar.classList.remove('background-light')
 
-                    if (navBar.classList.contains('border-dark-thin')) {
-                        navBar.classList.add('border-light-thin')
-                        navBar.classList.remove('border-dark-thin')
-                    }
+                if (navBar.classList.contains('border-dark-thin')) {
+                    navBar.classList.add('border-light-thin')
+                    navBar.classList.remove('border-dark-thin')
+                }
+            } else {
+                navBar.classList.add('background-light')
+                navBar.classList.remove('background-dark')
 
-                    break
-                
-                case false:
-                    navBar.classList.add('background-light')
-                    navBar.classList.remove('background-dark')
-
-                    if (navBar.classList.contains('border-light-thin')) {
-                        navBar.classList.add('border-dark-thin')
-                        navBar.classList.remove('border-light-thin')
-                    }
-
-                    break
+                if (navBar.classList.contains('border-light-thin')) {
+                    navBar.classList.add('border-dark-thin')
+                    navBar.classList.remove('border-light-thin')
+                }
             }
         }
     }
