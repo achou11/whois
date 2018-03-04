9e2bee9ed4d91f523c22534e57d149e7789ace8a
achou11 <chouandrewo@gmail.com>
2018-03-04 11:46:15 -0600

    remove commented old footer

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -8,7 +8,7 @@ module.exports = {
         return m('div.grid', [
             m(Nav),
             m('div.content-wrapper', m('div.content', vnode.children)),
-            m(Footer)//m('footer', m.trust('<span>Copyright &copy; 2018 Andrew Chou</span>'))
+            m(Footer)
         ])
     }
 }
